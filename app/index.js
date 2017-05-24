587a84f4d6f6f72030ec34df32222ea1d2a26ba1
YakumoAoi <416910533@qq.com>
2017-05-24 15:09:20 +0900

    task1 motified

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ import _ from 'lodash';
 import $ from 'jquery';
 
 function component() {
-    document.write("hello world")
+    document.write("hello webpack")
     console.log("方方的教程");
 }
 component();
